0717eab206d326e0cb511e68afe26d0713a6bc36
LuthfillahMafazi <luthfimafazi18@gmail.com>
2021-03-09 13:04:28 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/zii/flutter_food/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zii.flutter_food
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_food/food.dart';
+
+
+List<Food> dataFood = [
+  Food(
+      name: "Blueberry Cake", image: "blueberrycake.jpg", description: "Cake dengan selai blueberry lembut", price: "1.0000.000"),
+  Food(
+      name: "Cookie",
+      image: "cookie.jpg",
+      description: "Biskuit dengan rasa yang enak",
+      price: "1.0000.000"),
+  Food(
+      name: "Lasagna",
+      image: "lasagna.jpg",
+      description: "kue",
+      price: "1.0000.000"),
+
+];
